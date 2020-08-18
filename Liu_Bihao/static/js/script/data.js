59c0588800782b968165ca339a46b89b7a83db59
LiuPiPiPi <liu_bihao@163.com>
2020-08-18 10:50:14 +0800

    template practice

--- Liu_Bihao/static/js/script/data.js
+++ Liu_Bihao/static/js/script/data.js
@@ -161,3 +161,14 @@ $(function () {
         }
     });
 })
+
+/* 学习路线板块顶部标签展示 "curr"为被选中状态 */
+$(function () {
+    var str_learnRoad_tag_html = "";
+    var str_learnRoad_tags = ["热门", "前端", "后端", "移动端", "计算机基础", "大数据", "测试", "人工智能"]
+    str_learnRoad_tag_html += `<li class="curr">${str_learnRoad_tags[0]}</li>`
+    for (var i = 1; i < str_learnRoad_tags.length; i++) {
+        str_learnRoad_tag_html += `<li>${str_learnRoad_tags[i]}</li>`;
+    }
+    $(".main-learn-road ul").append(str_learnRoad_tag_html);
+})
