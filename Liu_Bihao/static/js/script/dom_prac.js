fb89cbdd221dff4a0261676dfdc483e2a8c6378b
LiuPiPiPi <liu_bihao@163.com>
2020-08-21 14:37:04 +0800

    practice of d3.js & echarts.js

--- Liu_Bihao/static/js/script/dom_prac.js
+++ Liu_Bihao/static/js/script/dom_prac.js
@@ -1,23 +1,26 @@
-var fontColors = document.getElementById("right_nav");
-var list = fontColors.querySelectorAll("span");
+function changeColorOfRightNav() {
+    let rightNavs = document.getElementById("right_nav");
+    let rightNavs_spanLists = rightNavs.querySelectorAll("span");
 
-for (var j = 0; j < list.length; j++) {
-    list[j].index = j;
+    for (let i = 0; i < rightNavs_spanLists.length; i++) {
+        rightNavs_spanLists[i].index = i;
+    }
+
+    rightNavs.addEventListener('mouseover', function (event) {
+        /* event.target 指被执行事件的 DOM 对象
+         * event.currentTarget 的区别是：若无监听事件，则向上追溯 DOM 对象
+         */
+        let no_ = event.target.index;
+        let obj = document.getElementById("right_nav").getElementsByTagName("span")[no_];
+        obj.style.color = "lightskyblue";
+        obj.style.fontWeight = "bolder";
+    })
+
+    rightNavs.addEventListener('mouseout', function (event) {
+        let no_ = event.target.index;
+        let obj = document.getElementById("right_nav").getElementsByTagName("span")[no_];
+        obj.style.color = "black";
+        obj.style.fontWeight = "unset";
+    })
 }
 
-fontColors.addEventListener('mouseover', function (event) {
-    /* event.target 指被执行事件的 DOM 对象
-     * event.currentTarget 的区别是：若无监听事件，则向上追溯 DOM 对象
-     */
-    var no_ = event.target.index;
-    var obj = document.getElementById("right_nav").getElementsByTagName("span")[no_];
-    obj.style.color = "lightskyblue";
-    obj.style.fontWeight = "bolder";
-})
-
-fontColors.addEventListener('mouseout', function (event) {
-    var no_ = event.target.index;
-    var obj = document.getElementById("right_nav").getElementsByTagName("span")[no_];
-    obj.style.color = "black";
-    obj.style.fontWeight = "unset";
-})
