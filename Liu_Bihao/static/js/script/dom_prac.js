28a56b9a1e35c1c304997e40d48dfb727de35205
LiuPiPiPi <liu_bihao@163.com>
2020-08-24 18:05:32 +0800

    08-24 liu commit

--- Liu_Bihao/static/js/script/dom_prac.js
+++ Liu_Bihao/static/js/script/dom_prac.js
@@ -7,20 +7,20 @@ function changeColorOfRightNav() {
     }
 
     rightNavs.addEventListener('mouseover', function (event) {
-        /* event.target 指被执行事件的 DOM 对象
-         * event.currentTarget 的区别是：若无监听事件，则向上追溯 DOM 对象
+        /* event.target 指事件的目标
+         * event.currentTarget 指其事件处理程序当前正在处理事件的那个元素
          */
         let no_ = event.target.index;
         let obj = document.getElementById("right_nav").getElementsByTagName("span")[no_];
         obj.style.color = "lightskyblue";
         obj.style.fontWeight = "bolder";
-    })
+    }, false);
 
     rightNavs.addEventListener('mouseout', function (event) {
         let no_ = event.target.index;
         let obj = document.getElementById("right_nav").getElementsByTagName("span")[no_];
         obj.style.color = "black";
         obj.style.fontWeight = "unset";
-    })
+    }, false);
 }
 
