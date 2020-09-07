0cbc92b995c9f19a59bf3d9737f88da813a2b410
hunter <110793286@qq.com>
2020-09-07 17:56:28 +0800

    登录界面设计

--- Yang_Liuhantao/js/dom_event.js
+++ Yang_Liuhantao/js/dom_event.js
@@ -1,6 +1,6 @@
 var element = document.getElementById("nav_item");
 var all_li = element.getElementsByTagName("li");
-console.log(all_li[0]);
+// console.log(all_li[0]);
 
 for (var i = 0; i < all_li.length; i++){
     all_li[i].addEventListener("mouseover",function () {
@@ -13,7 +13,7 @@ for (var i = 0; i < all_li.length; i++){
 
     all_li[i].addEventListener("mouseout",function () {
         var all_a = this.getElementsByTagName("a");
-        console.log(all_a)
+        // console.log(all_a)
         for (var ele in all_a) {
             all_a[ele].style.color = "#8e8c8c";
         }
